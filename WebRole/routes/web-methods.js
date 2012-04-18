b55998809acc003b7a321eef28611e2487c00ebd
Dan Attfield <daniel.attfield@statpro.com>
2012-04-18 10:19:44 +0100

    Added forgotten files missed in last commit.

---
+++ WebRole/routes/web-methods.js
@@ -0,0 +1,13 @@
+﻿// ------------------------------------------
+// WEB METHOD ROUTING
+// ------------------------------------------
+
+// NOTE: These methods are distinct from the routes in routes/index.js in that
+// they do not attempt to render views when called; instead, they return data objects.
+
+// Local Authentication Confirmation
+exports.isUserAuthenticated = function (req, res) {
+    // If the token variable in the session is not truthy, we 
+    // haven't got one, and the user is not authenticated.
+    res.json({ authenticated: !req.session.token });
+};
