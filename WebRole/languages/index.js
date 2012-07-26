807ef60601a190c27b15a6010e3a190553a3a929
Asa Cristall Fama <asa.fama@statpro.com>
2012-07-26 12:18:24 +0100

    Fixed a minor bug.

--- WebRole/languages/index.js
+++ WebRole/languages/index.js
@@ -1,3 +1,5 @@
 ﻿exports['en-US'] = require("./en-US");
 exports['it-IT'] = require("./it-IT");
 exports['pl-PL'] = require("./pl-PL");
+exports['de-DE'] = require("./de-DE");
+exports['es-ES'] = require("./es-ES");
