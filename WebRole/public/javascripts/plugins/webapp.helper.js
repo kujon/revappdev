ee7ff25f7cbd3b42b2431cbff683d207e0049580
Asa Cristall Fama <asa.fama@statpro.com>
2012-07-30 17:16:34 +0100

    Added StringBuilder object and string format method to the helper.

--- WebRole/public/javascripts/plugins/webapp.helper.js
+++ WebRole/public/javascripts/plugins/webapp.helper.js
@@ -21,6 +21,71 @@ WebAppLoader.addModule({ name: 'helper', isPlugin: true }, function () {
         return str.match(suffix + "$") == suffix;
     }
 
+    function format () {
+      var s = arguments[0];
+      
+      for (var i = 0; i < arguments.length - 1; i++) {       
+        var reg = new RegExp("\\{" + i + "\\}", "gm");             
+        s = s.replace(reg, arguments[i + 1]);
+      }
+
+      return s;
+    }
+
+    function StringBuilder()
+    {
+	    var strings = [];
+
+	    this.append = function ()
+	    {
+            string = format.apply(null, arguments);
+
+		    string = verify(string);
+		    if (string.length > 0) strings[strings.length] = string;
+	    };
+
+        // Alias.
+        this.add = this.append;
+
+	    this.appendLine = function (string)
+	    {
+		    string = verify(string);
+		    if (this.isEmpty())
+		    {
+			    if (string.length > 0) strings[strings.length] = string;
+			    else return;
+		    }
+		    else strings[strings.length] = string.length > 0 ? "\r\n" + string : "\r\n";
+	    };
+
+	    this.clear = function () { strings = []; };
+
+	    this.isEmpty = function () { return strings.length == 0; };
+
+	    this.toString = function () { return strings.join(""); };
+
+	    var verify = function (string)
+	    {
+		    if (!defined(string)) return "";
+		    if (getType(string) != getType(new String())) return String(string);
+		    return string;
+	    };
+
+	    var defined = function (el)
+	    {
+		    // Changed per Ryan O'Hara's comment:
+		    return el != null && typeof(el) != "undefined";
+	    };
+
+	    var getType = function (instance)
+	    {
+		    if (!defined(instance.constructor)) throw Error("Unexpected object type");
+		    var type = String(instance.constructor).match(/function\s+(\w+)/);
+
+		    return defined(type) ? type[1] : "undefined";
+	    };
+    };
+
     // ------------------------------------------
     // DATA TYPE FUNCTIONS
     // ------------------------------------------
@@ -162,6 +227,8 @@ WebAppLoader.addModule({ name: 'helper', isPlugin: true }, function () {
     helper.getValueAs = getValueAs;
     helper.startsWith = startsWith;
     helper.endsWith = endsWith;
+    helper.format = format;
+    helper.StringBuilder = StringBuilder;
     helper.getType = getType;
     helper.hasValue = hasValue;
     helper.createUUID = createUUID;
