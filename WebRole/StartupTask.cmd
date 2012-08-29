87ebee1719d1eb1ddf9d0f2e63a4963424a72f4b
Dan Attfield <daniel.attfield@statpro.com>
2012-08-29 15:26:50 +0100

    Added StatupTask.cmd.

---
+++ WebRole/StartupTask.cmd
@@ -0,0 +1,12 @@
+echo Copying web.cloud.config to web.config...
+copy /y Web.cloud.config Web.config
+if %ERRORLEVEL% neq 0 goto error
+echo OK
+
+echo SUCCESS
+exit /b 0
+
+:error
+
+echo FAILED
+exit /b -1
